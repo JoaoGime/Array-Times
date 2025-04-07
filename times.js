75e3a073605e6788dc8f855e84fbc64fa8e153e4
João Gimenes <joaopedrossgg@gmail.com>
2025-04-07 10:57:27 -0300

    Add files via upload

---
+++ times.js
@@ -0,0 +1,6 @@
+var times = ["São Paulo", "Corinthians" , "Palmeiras" , "Santos" , "Bahia" , "Fortaleza" , "Amazonas" , "Vasco" , "Fluminense" ,"Flamengo" , "Vitoria" , "Cruzeiro" , "Gremio" , "Juventude" , "Sport" , "Mirasol"] 
+
+var timesEscolhido = parseInt(prompt("Digite um número de 0 a 15"))
+
+ 
+document.write("Nome escolhido: " + times[timesEscolhido])
